7d870b6447ee1a5e4edb07c369003af97e54c2f2
Falak Khan <falak7121khan@gmail.in>
2025-11-18 18:44:30 +0530

    Fix render path

--- controllers/listings.js
+++ controllers/listings.js
@@ -2,7 +2,7 @@ const Listing = require("../models/listing");
 
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings, category:null});
+    res.render("listings/index", {allListings, category:null});
 };
 module.exports.renderNewForm = (req,res) => {
     res.render("new.ejs")
